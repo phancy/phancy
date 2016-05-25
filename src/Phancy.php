7760b0133e618792dee44681d65c565dabd3f6f1
Lark Mullins <larkmullins@gmail.com>
2016-05-25 15:47:40 -0500

    Added __call for Phancy's HTTP verb methods. Added dispatcher invocation

--- src/Phancy.php
+++ src/Phancy.php
@@ -2,17 +2,21 @@
 namespace Phancy;
 
 use Phancy\Interfaces\Serializer;
+use Phancy\Routing\Router;
 
 class Phancy {
+    private $router;
     private $resources;
     private $request;
     private $response;
+    private $verbs = ['DELETE', 'GET', 'PATCH', 'POST', 'PUT'];
 
     public function __construct()
     {
         $this->resources = [];
-        $this->request = new Http\Request();
-        $this->response = new Http\Response();
+        $this->request = new Http\Request;
+        $this->response = new Http\Response;
+        $this->router = new Routing\Router;
     }
 
     public function register(Interfaces\Resource $resource)
@@ -23,15 +27,28 @@ class Phancy {
     public function process(Serializer $serializer = null)
     {
         foreach ($this->resources as $resource) {
-            $resource->endpoints();
+            $resource->endpoints($this);
         }
 
-        // TODO: dispatch and invoke closure with request and response
+        // dispatch routes
+        $dispatcher = new Routing\Dispatcher($this->router);
+        $data = $dispatcher->dispatch();
 
         if ($serializer === null) {
-            $serializer = new Serializers\JsonSerializer();
+            $serializer = new Serializers\JsonSerializer($data);
         }
 
         $this->response->send($serializer);
+    }
+
+    public function __call($verb, $params)
+    {
+      if (in_array($verb, $this->verbs)) {
+          // $params[0] is the route's endpoint
+          // $params[1] is the route's handler
+          $this->router->addRoute($verb, $params[0], $params[1]);
+      } else {
+          thrown new \Exception('Method not found');
+      }
     }
 }
